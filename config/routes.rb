82203934246260be6c8f61bef5a2468f75fc5fd7
Angy Hijaze <angy.hijaze@gmail.com>
2024-12-03 18:03:22 +0100

    fixed review index page

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "/animals/:id", to: "animals#show"
-  get "/bookings/:booking_id/reviews", to: "bookings#index"
   #  get "bookings/:id/review", to: "reviews/show"
   # Defines the root path route ("/")
   # root "posts#index"
