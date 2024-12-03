cd3f5700640653f26124e229e5ced5c1be41eadf
Eliot Lila <147817846+EliotL1@users.noreply.github.com>
2024-12-03 15:40:08 +0100

    added reviews, adjust styling

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,7 @@
 class Animal < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many :reviews, through: :bookings
   has_many :users, through: :bookings
   validates :name, :species, :age, :price, :availability, :photos, presence: true
   validates :description, presence: true, length: { minimum: 12 }
