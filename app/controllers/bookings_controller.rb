befa7294ba03a47e82629b8bc79341dd137655f2
Violeta Petkovic <v.petkovic@protonmail.com>
2024-12-03 15:25:06 +0100

    adds come basic styling to the bookings index

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,8 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
-    # REPLACE JOHN WITH current_user
-    @john = User.first
-    @current_user = @john
-    # same as current_user.bookings but makes one animal query for each user instead of several
-    @made_bookings = @current_user.bookings.includes(:animal)
-    # same here with query
-    @received_bookings = @current_user.received_bookings.includes(:user, :animal)
+    @made_bookings = current_user.bookings.includes(:animal)
+    @received_bookings = current_user.received_bookings.includes(:user, :animal)
   end
 end
