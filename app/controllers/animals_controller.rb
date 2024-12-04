2eeba7cacd77186e0d32007b3c0fef857cb13208
Violeta Petkovic <v.petkovic@protonmail.com>
2024-12-04 12:43:03 +0100

    completes cancel booking & viewing all listings, works on deleting listing

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,32 +1,29 @@
 class AnimalsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_animal, only: [:show, :destroy]
 
   def index
     @animals = Animal.all
+  end
+
+  def my_listings
+    @animals = current_user.animals
   end
 
   def show
-    @animal = Animal.find(params[:id])
-  end
-
-  def index
-    @animals = Animal.all
+    # empty
   end
 
   def new
     @animal = Animal.new()
   end
 
-  def index
-    @animals = Animal.all
-  end
-
-
   def create
     @animal = current_user.animals.new(animal_params)
 
     @available_start = Date.parse(params[:animal][:available_start])
     @available_end = Date.parse(params[:animal][:available_end])
+
     if @available_start && @available_end
       @animal.availability = Date.today >= @available_start && Date.today <= @available_end
     else
@@ -41,7 +38,17 @@ class AnimalsController < ApplicationController
     end
   end
 
+  def destroy
+    raise
+    @animal.destroy
+    redirect_to my_listings_path, status: :see_other
+  end
+
   private
+
+  def set_animal
+    @animal = Animal.find(params[:id])
+  end
 
   def animal_params
     params.require(:animal).permit(:name, :species, :age, :price, :availability, :description, photos: [])
